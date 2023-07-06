bd92efd4e45b5925d3e3fc2c55e2864f00bc00e5
jonathanpenaloa <jona_pena_91@outlook.com>
2023-07-06 10:26:11 -0700

    db connected and client running

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+require("./config/database.js");
+
+const PORT = process.env.PORT || 3002
+ 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
