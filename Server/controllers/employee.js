fbc7bd19104fe910415363b12f135f420e1c2400
jonathanpenaloa <jona_pena_91@outlook.com>
2023-07-11 23:37:33 -0700

    added populate method to employee controller functions

--- Server/controllers/employee.js
+++ Server/controllers/employee.js
@@ -3,7 +3,8 @@ const Employee = require("../models/employee");
 const createEmployee = async(req, res) => {
     const employee = req.body;
     try {
-        const response = await Employee.create(employee);
+        const response = await Employee.create(employee)
+        .populate("hiredBy").sort("-createAt").exec();
         res.send(response)
     } catch(err) {
         res.send().status(500).json("Employee was not added")
@@ -14,7 +15,9 @@ const createEmployee = async(req, res) => {
     const employeeId = req.params.employeeId;
     const changesForEmployee = req.body;
     try {
-        const updatedEmployeeInfo = await Employee.findByIdAndUpdate(employeeId, changesForEmployee, { new : true });
+        const updatedEmployeeInfo = 
+        await Employee.findByIdAndUpdate(employeeId, changesForEmployee, { new : true })
+        .populate("hiredBy").sort("-createAt").exec();
         res.send(updatedEmployeeInfo);
     } catch(err) {
         res.send("employee was not updated").status(500);
@@ -23,12 +26,14 @@ const createEmployee = async(req, res) => {
 
  const deleteEmployee = async (req, res) => {
     const employeeId = req.params.employeeId;
-    const deletedEmployee = await Employee.findOneAndDelete(employeeId);
+    const deletedEmployee = await Employee.findOneAndDelete(employeeId)
+    .populate("hiredBy").sort("-createAt").exec();
     res.send(deletedEmployee); 
  }
 
  const getAllEmployees = async (req, res) => {
-    const employeeList = await Employee.find({}).populate("hiredBy").sort("-createAt").exec();
+    const employeeList = await Employee.find({})
+    .populate("hiredBy").sort("-createAt").exec();
     res.send(employeeList);
  }
  module.exports = {
